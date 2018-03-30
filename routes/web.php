6236a9d49635a5dc503161bd7c0acb8696e88c42
Marco Antonio Andrade <marco.andrade@ifro.edu.br>
2018-03-30 12:38:03 -0400

    CRUD completo

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/produtos/pesquisar', 'ProdutoController@pesquisar');
+Route::post('/produtos/pesquisar', 'ProdutoController@pesquisar');
+
+Route::get('/produtos/inserir', 'ProdutoController@mostrar_inserir');
+Route::post('/produtos/inserir', 'ProdutoController@inserir');
+
+Route::get('/produtos/alterar/{id}', 'ProdutoController@mostrar_alterar');
+Route::post('/produtos/alterar', 'ProdutoController@alterar');
+
+Route::get('/produtos/excluir/{id}', 'ProdutoController@excluir');
